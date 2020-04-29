2ce49499509125d70588fc5078eabd4c605b3991
Irupe <irupecanet@gmail.com>
2020-04-29 21:29:47 +0100

    second Commit

--- analysis.py
+++ analysis.py
@@ -1,10 +1,13 @@
+
+# Loading dataset
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import numpy as np
 col=['sepal_length','sepal_width','petal_length','petal_width','type']
 iris = pd.read_csv ("iris.csv",names=col)
 
+#Outputs of Shape and Size
 print ("First five rows")
 print(iris.head())
 #
@@ -18,20 +21,16 @@ print("no of  samples available for each type")
 print(iris["type"].value_counts())
 #
 print(iris.describe())
+#Observations: each type of iris flowers(3) it is = 50
 
+# Divide data
 
-plt.scatter (df['sepal_length'], df['sepal_width'])
-plt.title("Sepal length versus Sepal width")
-plt.xlabel("Sepal Length")
-plt.ylabel("Sepal Width")
+iris_setosa=iris.loc[iris["type"]=="Iris-setosa"]
+iris_virginica=iris.loc[iris["type"]=="Iris-virginica"]
+iris_versicolor=iris.loc[iris["type"]=="Iris-versicolor"]
+
+# histograms
+sns.FacetGrid(iris,hue="type",size=3).map(sns.distplot,"petal_length").add_legend()
 plt.show()
 
-plt.scatter (df['petal_length'], df['petal_width'])
-plt.title("Petal length versus Petal width")
-plt.xlabel("Petal Length")
-plt.ylabel("Petal Width")
-plt.show()
-
-sns.pairplot(df, hue="species")
-plt.show()
 
